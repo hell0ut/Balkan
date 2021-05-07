1ae861ad9a85fab91cd4d3b21944ad07ca513968
hell0ut <vlad.poplavsckyi@gmail.com>
2021-05-07 04:55:03 -0700

    sectdfs

--- Balkan/settings.py
+++ Balkan/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = settings.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
